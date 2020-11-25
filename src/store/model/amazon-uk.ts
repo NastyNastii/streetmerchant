c28206238c621119019b19168f41611f063e0e44
NastyNastii <43385998+NastyNastii@users.noreply.github.com>
2020-11-25 23:48:02 +0000

    Update amazon-uk.ts

--- src/store/model/amazon-uk.ts
+++ src/store/model/amazon-uk.ts
@@ -40,7 +40,7 @@ export const AmazonUk: Store = {
                         brand: 'Pokemon',
                         cartUrl: 'https://www.amazon.com/gp/aws/cart/add.html?ASIN.1=B083JX7P62&Quantity.1=1',
                         model: 'Toxtricity V',
-                        series: 'sonyps5c',
+                        series: 'pokemon',
                         url: 'https://www.amazon.co.uk/gp/product/B083JX7P62'
                 },
 		{
